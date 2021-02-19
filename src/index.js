0e77b51c543c000b3f4b4af753fd0defb76f20ff
vakhtangaA <vakhtang.chitauri@gmail.com>
2021-02-19 12:32:32 +0400

    font-awesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import "../node_modules/font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
